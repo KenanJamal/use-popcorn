1817c1f9a7c73bc1e44e7b145f50cd8001c77f37
kenan <kenanmjamal@gmail.com>
2026-02-14 20:46:57 +0300

    making the search bar to focus when i hit Enter using Ref

--- src/Search.js
+++ src/Search.js
@@ -1,4 +1,20 @@
+import { useEffect, useRef } from "react";
+
 export function Search({ query, setQuery }) {
+  const inputField = useRef(null);
+  useEffect(
+    function () {
+      function callback(e) {
+        if (document.activeElement === inputField.current) return;
+        if (e.code === "Enter") {
+          inputField.current.focus();
+          setQuery("");
+        }
+      }
+      document.addEventListener("keydown", callback);
+    },
+    [setQuery],
+  );
   return (
     <input
       className="search"
@@ -6,6 +22,7 @@ export function Search({ query, setQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => setQuery(e.target.value)}
+      ref={inputField}
     />
   );
 }
